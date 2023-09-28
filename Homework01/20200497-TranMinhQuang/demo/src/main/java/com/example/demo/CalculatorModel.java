7ede580fcbb087997a8bb10b7e33b4af1a8e259d
Cwvng <quangtrancnh@gmail.com>
2023-09-28 23:53:08 +0700

    homework1

---
+++ Homework01/20200497-TranMinhQuang/demo/src/main/java/com/example/demo/CalculatorModel.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class CalculatorModel {
+    private Double calculationValue;
+
+    public void addTwoNumbers(double firstNumber, double secondNumber){
+        calculationValue = firstNumber + secondNumber;
+
+    }
+    public void subTwoNumbers(double firstNumber, double secondNumber) {
+        calculationValue = firstNumber - secondNumber;
+    }
+
+    public void multiplyTwoNumber(double firstNumber, double secondNumber) {
+        calculationValue = firstNumber * secondNumber;
+    }
+
+    public void dividedTwoNumber(double firstNumber, double secondNumber) {
+        calculationValue = firstNumber / secondNumber;
+    }
+
+    public Double getCalculationValue() {
+        return calculationValue;
+    }
+}
